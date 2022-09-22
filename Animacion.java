f20382fcf108df0270add7a09dfa39c45d4aa2e1
BuhMo <corvus164@gmail.com>
2022-09-21 22:53:07 -0700

    animacion simulacion

--- Animacion.java
+++ Animacion.java
@@ -1,9 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Animacion extends JLabel implements Runnable{
+public class Animacion extends JLabel implements Runnable {
 
 	boolean pausar, reanudar, stop;
+	static String[] urlList1;
+	static ImageIcon img1;
+	static int y, max;
 
 	public Animacion(String name) {
 		setText(name);
@@ -11,20 +14,33 @@ public class Animacion extends JLabel implements Runnable{
 
 	public void run() {
 		stop = false;
-		for(int i=1; i<=10; i++) {
+		int index = 0;
+		max = urlList1.length-1;
+
+		for (int i = 10; i <= 410; i++) {
+			img1 = new ImageIcon(this.getClass().getResource(urlList1[index]));
+			if (index == max) {
+				index = 0;
+			} else {
+				index++;
+			}
+
+			setIcon(img1);
+			setBounds(i, y, 42, 42);
+
 			try {
-				synchronized(this) {
-					while(pausar) {
+				synchronized (this) {
+					while (pausar) {
 						wait();
 					}
-					if(stop) {
+					if (stop) {
 						break;
 					}
 				} // end sync
 
-				Thread.sleep(1000);
+				Thread.sleep(250);
 				setText(Integer.toString(i) + "hot dog");
-			} catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println("Hubo un error con la simulación!");
 			}
 		} // end for
@@ -33,7 +49,6 @@ public class Animacion extends JLabel implements Runnable{
 	synchronized void pausarHilo() {
 		pausar = true;
 	}
-
 
 	synchronized void reanudarHilo() {
 		pausar = false;
--- VistaSimulacion.java
+++ VistaSimulacion.java
@@ -5,12 +5,13 @@ import javax.swing.*;
 public class VistaSimulacion extends JPanel {
 	private JButton btnPause, btnRestart, btnStop;
 	private Animacion animacion;
-	//public JPanel parent; // Panel parent
-	
+	static String[] urlList1;
+	// public JPanel parent; // Panel parent
+
 	public VistaSimulacion() {
 		initValues();
 	} // end constructor
-	
+
 	// Método para iniciar la animación
 	public void start() {
 		Thread t1 = new Thread(animacion);
@@ -19,42 +20,48 @@ public class VistaSimulacion extends JPanel {
 		btnRestart.setEnabled(false);
 		btnStop.setEnabled(true);
 	}
-	
+
 	// Método que inicializa los elementos y propiedades de la ventana
 	public void initValues() {
 		Font fuenteTitulo = new Font("Courier New", 1, 52);
 		Font fuenteTexto = new Font("Courier New", 1, 16);
-		
+
 		// Crear elementos
 		animacion = new Animacion("S I M");
 		animacion.setFont(fuenteTitulo);
+		Animacion img1 = new Animacion("imagen1");
 		btnPause = new JButton("Pause");
-	        btnRestart = new JButton("Restart");
+		btnRestart = new JButton("Restart");
 		btnStop = new JButton("Stop");
 
+		urlList1 = new String[] {"images/hot-dog.png", "images/hot-dog2.png", "images/hot-dog3.png" };
+		img1.urlList1 = urlList1;
+		img1.y = 20;
+
 		// Definir propiedades de los elementos
-		animacion.setBounds(240,10,800,400);
-		btnPause.setBounds(340,500,200,100);
-		btnRestart.setBounds(540,500,200,100);
-		btnStop.setBounds(740,500,200,100);		
+		animacion.setBounds(240, 10, 800, 400);
+		btnPause.setBounds(340, 500, 200, 100);
+		btnRestart.setBounds(540, 500, 200, 100);
+		btnStop.setBounds(740, 500, 200, 100);
+		img1.setBounds(50,60,42,42);
 
 		ActionListener listener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == btnPause) {
-				    animacion.pausarHilo();
-				    btnPause.setEnabled(false);
-				    btnRestart.setEnabled(true);
+					animacion.pausarHilo();
+					btnPause.setEnabled(false);
+					btnRestart.setEnabled(true);
 				} // end btnPause
 				if (e.getSource() == btnRestart) {
-				    animacion.reanudarHilo();
-				    btnPause.setEnabled(true);
-				    btnRestart.setEnabled(false);
+					animacion.reanudarHilo();
+					btnPause.setEnabled(true);
+					btnRestart.setEnabled(false);
 				} // end btnRestart
 				if (e.getSource() == btnStop) {
-				    animacion.stopHilo();
-				    btnPause.setEnabled(false);
-				    btnRestart.setEnabled(false);
-				    btnStop.setEnabled(false);
+					animacion.stopHilo();
+					btnPause.setEnabled(false);
+					btnRestart.setEnabled(false);
+					btnStop.setEnabled(false);
 				} // end btnStop
 			} // end actionPerformed
 		};
@@ -69,11 +76,14 @@ public class VistaSimulacion extends JPanel {
 		add(btnPause);
 		add(btnRestart);
 		add(btnStop);
-		
+		//add(img1);
+
 		// Propiedades del panel
-		setPreferredSize(new Dimension(1280,720));
+		setPreferredSize(new Dimension(1280, 720));
 		setLayout(null);
 		setVisible(false);
+		Color fondo = new Color(130,130,130);
+		setBackground(fondo);
 	} // end initValues
-	
+
 }
