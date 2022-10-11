0e7b67cbf0d01056acc8a2ee15b6f5f445429fbe
Raúl Soto <72535875+rssv384@users.noreply.github.com>
2022-10-11 16:32:54 -0700

    Clase Recursos (setters+getters)

---
+++ Recursos.java
@@ -0,0 +1,49 @@
+public class Recursos {
+	
+	// Recursos disponibles para preparar hot dogs
+	private int panD, salchichaD, tocinoD;
+	private double tomateD, lechugaD, mayonesaD;
+
+	// Recursos utilizados en la preparación
+	private int panU, salchichaU, tocinoU;
+	private double tomateU, lechugaU, mayonesaU;
+
+	// ===== SETTERS Y GETTERS =====
+	// Recursos disponibles
+	public void setPanD(int panDisp) { this.panD = panDisp; }
+	public int getPanD() { return panD; }
+
+	public void setSalchichaD(int salchichaDisp) { this.salchichaD = salchichaDisp; }
+	public int getSalchichaD() { return salchichaD; }
+
+	public void setTocinoD(int tocinoDisp) { this.tocinoD = tocinoDisp; }
+	public int getTocinoD() { return tocinoD; }
+
+	public void setTomateD(double tomateDisp) { this.tomateD = tomateDisp; }
+	public double getTomateD() { return tomateD; }
+
+	public void setLechugaD(double lechugaDisp) { this.lechugaD = lechugaDisp; }
+	public double getLechugaD() { return lechugaD; }
+
+	public void setMayonesaD(double mayonesaDisp) { this.mayonesaD = mayonesaDisp; }
+	public doublw getMayonesaD() { return mayonesaD; }
+
+	// Recursos utlizados
+	public void setPanU(int panUtil) { this.panU = panUtil; }
+	public int getPanU() { return panU; }
+
+	public void setSalchichaU(int salchichaUtil) { this.salchichaU = salchichaUtil; }
+	public int getSalchichaU() { return salchichaU; }
+
+	public voiU setTocinoU(int tocinoUtil) { this.tocinoU = tocinoUtil; }
+	public int getTocinoU() { return tocinoU; }
+
+	public voiU setTomateU(Uouble tomateUtil) { this.tomateU = tomateUtil; }
+	public Uouble getTomateU() { return tomateU; }
+
+	public voiU setLechugaU(Uouble lechugaUtil) { this.lechugaU = lechugaUtil; }
+	public Uouble getLechugaU() { return lechugaU; }
+
+	public voiU setMayonesaU(Uouble mayonesaUtil) { this.mayonesaU = mayonesaUtil; }
+	public Uoublw getMayonesaU() { return mayonesaU; }
+}
